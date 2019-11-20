60a3a4e4c710ca712cadeae0ce0bcf5385ad1890
robro612 <robro612@gmail.com>
2019-11-20 02:04:48 -0500

    Testing LowPolyGenerator Class

--- test.py
+++ test.py
@@ -28,17 +28,20 @@ print("read",(time.time() - t) * 1000)
 
 t = time.time()
 gr_img = np.dot(img[:,:,:3], [.3, .6, .1])
+print("BROSBNSFBNSB,", gr_img)
 # plt.imshow(gr_img, cmap = "gray")
 # plt.show()
 #gr_img = img
-# print(gr_img.shape)
-gr_img = cv2.blur(img,(5,5))
+# print("NOJOKE" , gr_img.shape)
+gr_img = cv2.blur(img,(3,3))
+# print("blurred,", gr_img)
 gr_img = cv2.filter2D(gr_img,-1,kernel = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]]))
+# print("HOW", gr_img)
 edges = cv2.Canny(gr_img,1,100)
-print("BRO SRSYLT", type(gr_img))
+# print("BRO SRSYLT", type(gr_img))
 # print(img.shape,edges.shape)
 # print(type(edges))
-# print(edges[190,565])
+print("AHOY", edges[190,565])
 
 print("Canny",(time.time() - t) * 1000)
 
@@ -53,6 +56,7 @@ print(400/sampleRate)
 t = time.time()
 for row in range(edges.shape[0]):
     for col in range(edges.shape[1]):
+
         if edges[row, col] == 255:
             # actual points
             if random.random() > 0.8:
@@ -116,31 +120,31 @@ def runDrawing(width=img.shape[1], height=img.shape[0]):
     root.mainloop()
     print("bye!")
 
-runDrawing()
+#runDrawing()
 
-print(type(tri))
-print(tri.simplices[0])
-print("delaunay",(time.time() - t) * 1000)
-print(points[tri.simplices[5]])
-
-f = plt.figure()
-f.add_subplot(1,2,1)
-plt.imshow(edges, cmap = 'gray')
-f.add_subplot(1,2,2)
-plt.imshow(gr_img, cmap = 'gray')
-plt.show()
-
-print(edges[0][0])
-
-print(points[:,0].shape)
-#plt.triplot(points[:,0], points[:,1], tri.simplices.copy(), zorder=2)
-
-plt.scatter(points[:,0], points[:,1], zorder=2)
-plt.imshow(img, cmap = "gray", zorder=1)
-plt.show()
-
-
-print(type(img))
+# print(type(tri))
+# print(tri.simplices[0])
+# print("delaunay",(time.time() - t) * 1000)
+# print(points[tri.simplices[5]])
+#
+# f = plt.figure()
+# f.add_subplot(1,2,1)
+# plt.imshow(edges, cmap = 'gray')
+# f.add_subplot(1,2,2)
+# plt.imshow(gr_img, cmap = 'gray')
+# plt.show()
+#
+# print(edges[0][0])
+#
+# print(points[:,0].shape)
+# #plt.triplot(points[:,0], points[:,1], tri.simplices.copy(), zorder=2)
+#
+# plt.scatter(points[:,0], points[:,1], zorder=2)
+# plt.imshow(img, cmap = "gray", zorder=1)
+# plt.show()
+#
+#
+# print(type(img))
 
 """
 Workflow:
--- triangulator_class.py
+++ triangulator_class.py
@@ -14,15 +14,17 @@ import cv2, random, time
 # time to test runtime during testing
 
 class LowPolyGenerator():
-    def __init__(self, imagePath, blurSize=3, sharpen=True, grayScale=False,
+    def __init__(self, imagePath, blurSize=3, sharpen=True,
                 nodeSampleDistanceThreshold=10, randomNoiseRate=400,
-                cannyLow=10, cannyHigh=100, verbose=False,
+                cannyLow=50, cannyHigh=100, verbose=False,
                 saveResults=False):
         self.path = imagePath
         self.blurSize = blurSize
         self.sharpen = sharpen
         self.nodeSampleDistanceThreshold = nodeSampleDistanceThreshold
         self.randomNoiseRate = randomNoiseRate
+        self.cannyLow = cannyLow
+        self.cannyHigh = cannyHigh
         self.verbose = verbose
         self.saveResults = saveResults
         self.image = self.loadImage()
@@ -30,7 +32,7 @@ class LowPolyGenerator():
     #Loads image using matplotlib's imread method
     def loadImage(self):
         try:
-            img = imread(self.path)
+            image = imread(self.path)
         except:
             print("Image was not found in directory")
             return
@@ -43,23 +45,27 @@ class LowPolyGenerator():
     # filter for increased edge detection
     def preProcessImage(self):
         message = ""
-        if self.grayScale:
-            preProcessed = np.dot(self.image[:,:,:3], [0.3, 0.6, 0.1])
-            mgs += "Converting to grayscale \n"
+        preProcessed = np.dot(self.image[:, :, :3], [0.3, 0.6, 0.1])
+        message += "Converting to grayscale \n"
         if self.blurSize > 0:
-            preProcessed = cv2.blur(self.image, (self.blurSize, self.blurSize))
+            preProcessed = cv2.blur(self.image,
+            (self.blurSize, self.blurSize))
             message += \
-            f"Blurring with a normalized box filter of size {blurSize} \n"
+            f"Blurring with a normalized box filter of size {self.blurSize} \n"
+        else:
+            preProcessed = cv2.blur(self.image,
+            (1, 1))
         if self.sharpen:
-            sharpenKernel = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]]))
-            preProcessed = cv2.filter2D(self.image, kernel = sharpenKernel)
+            preProcessed = cv2.filter2D(preProcessed, -1,
+            kernel = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]]))
             message += "Sharpening \n"
         if self.verbose:
             print(message)
+        # print("BRUH", preProcessed.shape, preProcessed)
         return preProcessed
 
     # takes in two tuples, returns euclidean distance
-    @staticMethod
+    @staticmethod
     def distance(point1, point2):
         x1, y1 = point1
         x2, y2 = point2
@@ -70,34 +76,41 @@ class LowPolyGenerator():
     # returns a list of nodes in tuple form
     def edgeDetection(self):
         # uses cv2's canny edge detection filter
-        canny = cv2.Canny(image, self.cannyLow, self.cannyHigh)
+        #CHANGE THESE PARAMETERS BACK TO CANNYLOW AND HIGH
+        canny = cv2.Canny(self.preProcessed, self.cannyLow, self.cannyHigh)
         nodes = []
         threshold = self.nodeSampleDistanceThreshold
-        noiseProbability = self.randomNoiseRate/(img.shape[0]*img.shape[1])
-        for row in range(img.shape[0]):
-            for col in range(img.shape[1]):
-                if img[row, col] == 255:
+        noiseProbability = self.randomNoiseRate / \
+        (canny.shape[0]*canny.shape[1])
+        for row in range(canny.shape[0]):
+            for col in range(canny.shape[1]):
+                if canny[row, col] == 255 and random.random() < 100000/(self.image.shape[0]*self.image.shape[1]):
                     nodes.append((col, row))
                 elif random.random() < noiseProbability:
                     nodes.append((col, row))
         # removes nodes in nodeSampleDistanceThreshold distance
+        i = 0
+        count = 0
+        threshold = 2
         while i < len(nodes):
             j = i + 1
             while j < len(nodes):
-                if LowPolyGenerator.distance(nodes[i], nodes[j]) > threshold:
+                if random.random() > .99 and LowPolyGenerator.distance(nodes[i], nodes[j]) < threshold:
+                    count += 1
                     nodes.pop(j)
                 else:
                     j += 1
+            if i % 100 == 0:
             i += 1
+        print(f"{count} nodes were within {threshold} of each other")
         # adds the corners to ensure the complete space
-        for point in [(0,0), (0,img.shape[0]), (img.shape[1],0),
-                    (img.shape[1], (img.shape[0])]:
+        for point in [(0,0), (0,self.image.shape[0]), (self.image.shape[1],0),
+        (self.image.shape[1], self.image.shape[0])]:
             if point not in nodes:
                 nodes.append(point)
-        return nodes
+        return nodes, canny
 
-    @staticMethod
-    def getAverageColor(simplex):
+    def getAverageColor(self, simplex):
         # each simplex is a three-list of the indices of points from the passed
         # point array that make a given triangle
         nodes = self.nodes
@@ -114,18 +127,19 @@ class LowPolyGenerator():
 
     # RGB conversion formula from
     # http://www.cs.cmu.edu/~112/notes/notes-graphics-part2.html
-    @staticMethod
+    @staticmethod
     def rgbString(r,g,b):
-        return "#%02x%02x%02x" % (red, green, blue)
+        return "#%02x%02x%02x" % (r, g, b)
 
     # uses scipy's implementation of Delaunay triangulation
     def triangulate(self):
         delaunay = Delaunay(self.nodes)
         simplices = delaunay.simplices
         triangles = dict()
-        # iterates for each pair of three vertices of a given triangle
+        # iterates for each triple vertices of a given triangle
         for simplex in simplices:
-            triangles[simplex] = self.rgbString(self.getAverageColor(simplex))
+            r,g,b = self.getAverageColor(simplex)
+            triangles[tuple(simplex)] = self.rgbString(r,g,b)
         return triangles, delaunay
 
     # Wrapper method call that returns all relevant information including
@@ -134,9 +148,42 @@ class LowPolyGenerator():
     def generateTriangulation(self):
         start = time.time()
         self.preProcessed = self.preProcessImage()
-        self.nodes = self.edgeDetection()
+        self.nodes, self.canny = self.edgeDetection()
         self.triangles, self.delaunay = self.triangulate()
         end = time.time()
         if self.verbose:
             print(f"Time to generate: {end-start}")
         return self.triangles, self.delaunay, self.image, self.path
+
+lowPolyGenerator = LowPolyGenerator("/Users/rohanmjha/Desktop/College/15-112/term-project/images/nico.jpg")
+
+def draw(canvas, width, height, triangles, nodes):
+    canvas.create_rectangle(0,0, width, height, fill = "black")
+    for simplex in triangles:
+        x0,y0 = nodes[simplex[0]][0], nodes[simplex[0]][1]
+        x1,y1 = nodes[simplex[1]][0], nodes[simplex[1]][1]
+        x2,y2 = nodes[simplex[2]][0], nodes[simplex[2]][1]
+        canvas.create_polygon(x0,y0,x1,y1,x2,y2, fill = triangles[simplex],
+        width = 0)
+        # add outline = colorString to get rid of thin black outlines
+
+def runDrawing(lowPolyGenerator):
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+    canvas = Canvas(root, width = lowPolyGenerator.image.shape[1],
+    height = lowPolyGenerator.image.shape[0])
+    canvas.configure(bd=0, highlightthickness=0)
+    canvas.pack()
+    draw(canvas, lowPolyGenerator.image.shape[1],
+    lowPolyGenerator.image.shape[0], lowPolyGenerator.triangles,
+    lowPolyGenerator.nodes)
+    root.mainloop()
+    print("bye!")
+
+path = "/Users/rohanmjha/Desktop/College/15-112/term-project/images/nico.jpg"
+lowPolyGenerator = LowPolyGenerator(path)
+lowPolyGenerator.generateTriangulation()
+runDrawing(lowPolyGenerator)
+
+plt.imshow(lowPolyGenerator.canny)
+plt.show()
